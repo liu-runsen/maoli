202ce4052112c990469723bd76f920033a492a0e
MaoliRUNsen <45328088+MaoliRUNsen@users.noreply.github.com>
2019-09-10 23:25:51 +0800

    有道云翻译

--- 爬虫/demo.py
+++ 爬虫/demo.py
@@ -1,106 +1,55 @@
-# -*- coding：utf-8 -*-
-# time ：2019/3/29 17:09
-# author: 毛利
+# PROJECT： Reptile-master
+# File: demo.py
+# author： MaoLi (刘润森）
+# DATE：  2019/9/9 22:44 
 
-import re
-import base64
+
 import requests
+import hashlib
 import time
-import json
-import rsa
-from binascii import b2a_hex
+import random
 
-class WeiBo(object):
-    def __init__(self,username,password):
-        self.password = password
-        self.username = username
-        self.session = requests.session()
-    def encrypt_username(self):
-        '''
-        对账号进行base64加密
-        :return:
-        '''
-        return base64.b64encode(self.username.encode())
-
-    def encrypt_password(self):
-        '''
-        对密码进行rsa加密
-        :return:
-        '''
-        result = self.per_login()
-        my_password = str(result['servertime']) + '\t' + str(result['nonce']) + '\n' + str(self.password)
-        publicky = rsa.PublicKey(int(result['pubkey'],16),int('10001',16))
-        return b2a_hex(rsa.encrypt(my_password.encode(),publicky))
-
-    def per_login(self):
-        '''
-        预请求拿到加密的参数
-        :return:
-        '''
-        params = {
-            'entry': 'weibo',
-            'callback': 'sinaSSOController.preloginCallBack',
-            'su': self.encrypt_username(),
-            'rsakt': 'mod',
-            'checkpin': '1',
-            'client': 'ssologin.js(v1.4.19)',
-            '_': int(time.time()*1000)
+class fanyi(object):
+    def __init__(self):
+        self.headers= {
+            'Cookie': 'OUTFOX_SEARCH_USER_ID=-116786429@223.73.205.91; OUTFOX_SEARCH_USER_ID_NCOO=997889835.4626652; UM_distinctid=16be994bb1e142-0b7813bc2c777f-6353160-1fa400-16be994bb1f383; _ntes_nnid=1fe47fb7a39517ce84391a74d23c0644,1563159293320; _ga=GA1.2.1925019515.1563522506; JSESSIONID=aaa2M2sJpdWFq8kh_Ew0w; ___rl__test__cookies=1568042786896',
+            'Referer':'http://fanyi.youdao.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
         }
-        url = 'https://login.sina.com.cn/sso/prelogin.php'
-        response = self.session.get(url,params=params,verify = False)
-        # print(response.text)
-        json_data = re.findall(r'preloginCallBack\((.*?)\)',response.text,re.S)
-        # print(json_data)
-        if json_data:
-            print('预请求已拿到加密的参数')
-            return json.loads(json_data[0])
-        else:
-            print('预请求失败')
-    def login(self):
-        '''
-        提交post的数据
-        :return:
-        '''
-        result = self.per_login()
-        print(result)
-        data = {
-            'entry': 'weibo',
-            'gateway': '1',
-            'from':'',
-            'savestate':'7',
-            'qrcode_flag': 'false',
-            'useticket': '1',
-            'pagerefer':'https://login.sina.com.cn/crossdomain2.php?action=logout&r=https%3A%2F%2Fpassport.weibo.com%2Fwbsso%2Flogout%3Fr%3Dhttps%253A%252F%252Fweibo.com%26returntype%3D1',
-            'vsnf': '1',
-            'su': self.encrypt_username(),
-            'service': 'miniblog',
-            'servertime':result.get('servertime'),
-            'nonce': result.get('nonce'),
-            'pwencode': 'rsa2',
-            'rsakv': result.get('rsakv'),
-            'sp': self.encrypt_password(),
-            'sr': '1920 * 1080',
-            'encoding': 'UTF - 8',
-            'prelt':' 63',
-            'url': 'https://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
-            'returntype': 'META'
+        self.data = {
+            'i': None,
+            'from': 'AUTO',
+            'to': 'AUTO',
+            'smartresult': 'dict',
+            'client': 'fanyideskweb',
+            'salt': None,
+            'sign': None,
+            'ts': None,
+            'bv': None,
+            'doctype': 'json',
+            'version': '2.1',
+            'keyfrom': 'fanyi.web',
+            'action': 'FY_BY_REALTlME'
         }
-        post_url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)'
-        login_data = self.session.post(post_url,data=data)
-        login_data.encoding = 'gbk'
-        if login_data.status_code == 200:
-            next_url = re.findall(r'location.replace\("(.*?)"\)',login_data.text,re.S)
-            if next_url:
-                response = self.session.get(next_url[0]).content.decode('gbk')
-                print(response)
-                print('正在登陆')
-                self.session.get(re.findall(r"location.replace\('(.*?)'\)",response,re.S)[0])
-            print(self.session.get('https://www.weibo.com').text)
-            print('你的账号{}已成功登录'.format(self.username))
-
-
+        self.url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+    def fanyi(self,word):
+        salt  = str(int(time.time()*10000)+random.random()*10)
+        ts =  salt[:-1]
+        sign_notmd5 = 'fanyideskweb' + word + salt + 'n%A-rKaT5fb[Gy?;N5@Tj'
+        sign = hashlib.md5(sign_notmd5.encode('utf-8')).hexdigest()
+        bv = hashlib.md5('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'.encode('utf-8')).hexdigest()
+        self.data['i'] = word
+        self.data['salt'] = salt
+        self.data['sign'] = sign
+        self.data['ts'] = ts
+        self.data['bv'] = bv
+        res = requests.post(self.url, headers=self.headers, data=self.data)
+        return res.json()['translateResult'][0][0]['tgt']
 if __name__ == '__main__':
-    username = input('你的账号是：')
-    password = input('账号的密码是：')
-    weibo = WeiBo(username,password)
-    weibo.login()
+    youdao = fanyi()
+    while True:
+        content = input("请输入您需要翻译的内容:")
+        if content == "q":
+            break
+        res = youdao.fanyi(content)
+        print(res)
